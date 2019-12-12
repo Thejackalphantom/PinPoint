1af71648170c12ed043ef30907ef21f61f3900c2
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2019-12-12 13:06:26 +0100

    Removed button, possible issue is the splitting

--- FTDIapplication/app/src/main/java/com/example/ftdiapplication/MainActivity.java
+++ FTDIapplication/app/src/main/java/com/example/ftdiapplication/MainActivity.java
@@ -17,7 +17,7 @@ import com.example.ftdiapplication.USBSerialListener;
 import com.example.ftdiapplication.Utilities;
 
 import java.util.Arrays;
-
+import java.util.regex.Pattern;
 public class MainActivity extends AppCompatActivity implements USBSerialListener {
 
     USBSerialConnector mcConnector;
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements USBSerialListener
     EditText rxText;
     ImageView yesData;
     ImageView noData;
-    Button btSend;
     String[] dataArray;
 
     @Override
@@ -37,50 +36,52 @@ public class MainActivity extends AppCompatActivity implements USBSerialListener
         rxText = (EditText) findViewById(R.id.rxText);
         yesData = (ImageView) findViewById(R.id.yesData);
         noData = (ImageView) findViewById(R.id.noData);
-        btSend = (Button) findViewById(R.id.btSend);
         txText.setEnabled(false);
         rxText.setEnabled(false);
         yesData.setEnabled(false);
         noData.setEnabled(false);
-        btSend.setEnabled(false);
         /*Data array that will contain the data recieved through the serial antennae.
           The array will always have the following index: 0 is tab,
           1 is inclination, 2 is azimuth degrees, 3 is heightside.
         */
         dataArray = new String[4];
-        btSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String data = txText.getText().toString();
-                mcConnector.writeAsync(data.getBytes());
-            }
-        });
     }
 
     @Override
     public void onDataReceived (byte[] data) {
-        //TODO: Fix java.io.IOException: Already Closed
+        //TODO: Fix java.io.IOException: Already Closed &
         if (data != null) {
             //Set the recieving data icon to visible
             yesData.setVisibility(View.VISIBLE);
             //Set no data recieved icon to invisible
             noData.setVisibility(View.INVISIBLE);
             //Current issue: Below code causes severe errors and instability
-            //Empty the string array to prepare it to recieve new data
-            Arrays.fill(dataArray, null);
             //Split data string to fill in the array
-            dataArray = Utilities.bytesToString(data).split(",");
-            //For now, recreate string in array
-            String reconString;
-            reconString = dataArray[0] + dataArray[1] + dataArray[2] + dataArray[3];
-            //Display string in app.
-            rxText.setText(reconString + System.getProperty("line.separator"));
+            String dataString = Utilities.bytesToString(data);
+            writeDataToApp(dataString);
+
         } else {
             //Set no data recieved icon to visible
             noData.setVisibility(View.VISIBLE);
             //Set the recieving data icon to invisible
             yesData.setVisibility(View.INVISIBLE);
         }
+    }
+
+    public void writeDataToApp (String data)
+    {
+        //Create an array of the data through a split.
+        //Pattern.quote is utility
+        dataArray = data.split(Pattern.quote(","));
+
+        //Seperate the array into strings
+        String tab = dataArray[0];
+        String inc = dataArray[1];
+        String azi = dataArray[2];
+        String his = dataArray[3];
+
+        //Write array into app
+        rxText.setText(inc);
     }
 
     @Override
@@ -94,7 +95,6 @@ public class MainActivity extends AppCompatActivity implements USBSerialListener
         rxText.setEnabled(true);
         yesData.setEnabled(true);
         noData.setEnabled(true);
-        btSend.setEnabled(true);
     }
 
     @Override
