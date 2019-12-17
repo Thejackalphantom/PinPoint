80c5e452ba5469c8167c97b53fd00bf9a9218854
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2019-12-17 11:28:22 +0100

    Update MainActivity.java

--- FTDIapplication/app/src/main/java/com/example/ftdiapplication/MainActivity.java
+++ FTDIapplication/app/src/main/java/com/example/ftdiapplication/MainActivity.java
@@ -5,59 +5,49 @@ import android.os.Bundle;
 
 //android libraries
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 //the usb-serial Libraries
-import com.example.ftdiapplication.ResponseStatus;
-import com.example.ftdiapplication.USBSerialConnector;
-import com.example.ftdiapplication.USBSerialListener;
-import com.example.ftdiapplication.Utilities;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements USBSerialListener {
 
     USBSerialConnector mcConnector;
-    EditText txText;
-    EditText rxText;
+    EditText incText;
+    EditText aziText;
+    EditText hisText;
     ImageView yesData;
     ImageView noData;
-    ArrayList dataArray;
-    /*Data array that will contain the data recieved through the serial antennae.
-      The array will always have the following index: 0 is tab,
-      1 is inclination, 2 is azimuth degrees, 3 is heightside.
-    */
+    byte[] data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mcConnector = USBSerialConnector.getInstance();
-        txText = (EditText) findViewById(R.id.txText);
-        rxText = (EditText) findViewById(R.id.rxText);
+        incText = (EditText) findViewById(R.id.inclination);
+        aziText = (EditText) findViewById(R.id.azimuthDegrees);
+        hisText = (EditText) findViewById(R.id.heightside);
         yesData = (ImageView) findViewById(R.id.yesData);
         noData = (ImageView) findViewById(R.id.noData);
-        txText.setEnabled(false);
-        rxText.setEnabled(false);
+        incText.setEnabled(false);
+        aziText.setEnabled(false);
+        hisText.setEnabled(false);
         yesData.setEnabled(false);
         noData.setEnabled(false);
-        dataArray = new ArrayList<>();
     }
 
     @Override
-    public void onDataReceived (byte[] data) {
-        if (data != null) {
+    public void onDataReceived (byte[] incomingData) {
+        data = incomingData;
+        if (incomingData != null) {
             //Set the recieving data icon to visible
             yesData.setVisibility(View.VISIBLE);
             //Set no data recieved icon to invisible
             noData.setVisibility(View.INVISIBLE);
             //Current issue: Below code causes severe errors and instability
-            String dataString = Utilities.bytesToString(data);
-            writeDataToApp(dataString);
-//            rxText.setText(dataString);
+            writeDataToApp(data);
         } else {
             //Set no data recieved icon to visible
             noData.setVisibility(View.VISIBLE);
@@ -66,41 +56,28 @@ public class MainActivity extends AppCompatActivity implements USBSerialListener
         }
     }
 
-    public void writeDataToApp (String dataString)
+    public void writeDataToApp (byte[] recievedData)
     {
         //todo: Either fix or replace split to stop the crashes
-        String stringToAdd = "";
+        //Convert byte array to string
+        String dataString = Utilities.bytesToString(recievedData);
 
-        dataArray.clear();
-        //This is basically a really ugly split.
-        char[] dataCharArr = dataString.toCharArray();
-        for(char c : dataCharArr)
-        {
-            if(c != ",".toCharArray()[0])
-            {
-                stringToAdd += c;
-            }
-            else
-            {
-                dataArray.add(stringToAdd);
-                stringToAdd = "";
-            }
-        }
+        //These are the different string to be shown
+        String tab = "";
+        String inc = "";
+        String azi = "";
+        String his = "";
 
-        //Seperate the array into strings
-        String tab = dataArray.get(0).toString();
-        String inc = dataArray.get(1).toString();
-        String azi = dataArray.get(2).toString();
-        String his = dataArray.get(3).toString();
-
-        //Write array into app
-        rxText.setText(tab+" "+inc+" "+azi+""+his);
+        //setText writes the text to the app
+        aziText.setText(dataString);
+        incText.setText(inc);
+        hisText.setText(his);
     }
 
     @Override
     public void onDeviceReady(ResponseStatus responseStatus) {
-        txText.setEnabled(true);
-        rxText.setEnabled(true);
+        incText.setEnabled(true);
+        aziText.setEnabled(true);
         yesData.setEnabled(true);
         noData.setEnabled(true);
     }
