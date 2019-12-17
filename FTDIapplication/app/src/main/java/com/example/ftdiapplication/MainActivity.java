8c75ed0f97f5b7d7c9829db6fcf35f4e4576319a
Maurice Hoekstra <maurice.hoekstra@student.nhlstenden.com>
2019-12-17 12:45:07 +0100

    Written some comments

--- FTDIapplication/app/src/main/java/com/example/ftdiapplication/MainActivity.java
+++ FTDIapplication/app/src/main/java/com/example/ftdiapplication/MainActivity.java
@@ -12,26 +12,39 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements USBSerialListener {
 
+    //Class for the USBSerial Connector API
     USBSerialConnector mcConnector;
+    //Class for the field in the app where the inclination will be shown
     EditText incText;
+    //Class for the field in the app where the azimuth degree will be shown
     EditText aziText;
+    //Class for the field in the app where the high side degree will be shown
     EditText hisText;
+    //Class for the field in the app where the no incoming data display will be shown
     ImageView yesData;
+    //Class for the field in the app where the incoming data display will be shown
     ImageView noData;
+    //Class for the handler to handle the data processing code every second
     Handler Handler;
+    //Class that will run all the data processing code
     Runnable DataRunner;
+    //Byte-array that contains the recieved data from the FTDI chip
     byte[] data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Intialize the app layout
         setContentView(R.layout.activity_main);
+        //Intialize the USBSerialConnector api
         mcConnector = USBSerialConnector.getInstance();
+        //Get the ID of all the fields that require any manipulaton by code.
         incText = (EditText) findViewById(R.id.inclination);
         aziText = (EditText) findViewById(R.id.azimuthDegrees);
         hisText = (EditText) findViewById(R.id.heightside);
         yesData = (ImageView) findViewById(R.id.yesData);
         noData = (ImageView) findViewById(R.id.noData);
+        //Pre-emptively disable the fields for better showing, enabled with onDeviceReady()
         incText.setEnabled(false);
         aziText.setEnabled(false);
         hisText.setEnabled(false);
@@ -92,6 +105,7 @@ public class MainActivity extends AppCompatActivity implements USBSerialListener
 
     @Override
     public void onDeviceReady(ResponseStatus responseStatus) {
+        //This code is executed whenever the device is ready to roll
         incText.setEnabled(true);
         aziText.setEnabled(true);
         yesData.setEnabled(true);
@@ -100,11 +114,13 @@ public class MainActivity extends AppCompatActivity implements USBSerialListener
 
     @Override
     public void onDeviceDisconnected() {
+        //This is executed when the usb serial ftdi device is disconnected
         Toast.makeText(this, "Device disconnected", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onResume() {
+        //This is executed when the program resumes
         super.onResume();
         mcConnector.setUsbSerialListener(this);
         mcConnector.init(this, 9600);
@@ -112,6 +128,7 @@ public class MainActivity extends AppCompatActivity implements USBSerialListener
 
     @Override
     protected void onPause() {
+        //This is executed when the program is paused
         super.onPause();
         mcConnector.pausedActivity();
 
